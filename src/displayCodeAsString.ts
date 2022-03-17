9bfa951258b21db60513ceaa8e64711f65d2b00b
anderszeic <anderszeic@gmail.com>
2022-03-17 14:01:03 +0100

    Co-authored-by: Emilorzz <Emilorzz@users.noreply.github.com>

--- src/displayCodeAsString.ts
+++ src/displayCodeAsString.ts
@@ -1,4 +1,5 @@
-function printFunction(name: string){name = "Jesper hansen";
+function printFunction(name: string){
+    name = "Jesper hansen";
     let a : number = 7;
     let b : number = 6;
     console.log(a + b);
@@ -6,14 +7,23 @@ function printFunction(name: string){name = "Jesper hansen";
         a++;
     } 
     const c = a+b;
-} 
-let indent : number = 0;
+}
 
-let left = document.querySelector("#leftside")
+let left = document.querySelector("#left")
 let functionString : string = printFunction.toString();
 
-let stringArray = functionString.split(/(?<=\{\})|[\r\n]+/);
-console.log(stringArray);
+let stringArray : Array<String> = functionString.split(/(?<=\{\})|[\r\n]+/);
+//console.log(stringArray);
+
+let indentArray : Array<Number> = [];
+for(let i : number = 0; i<=stringArray.length ; i++){ 
+    let j : number = 1;
+    while (stringArray[i][j] === "a"){
+        j++;  
+    }
+    indentArray[i] = j;
+}
+console.log(indentArray);
 let paragraphString = stringArray.join("</p><p>");
 
 left.innerHTML = "<pre><p>" + paragraphString + "</p></pre>";
