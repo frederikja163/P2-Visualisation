530a1e62da2b31e2e02c342e898424a7a42a9bfe
FrederikJA <fjaplv64@gmail.com>
2022-03-18 09:51:46 +0100

    cleanup of breakpoint method

--- src/breakpoint.ts
+++ src/breakpoint.ts
@@ -1,15 +1,9 @@
 const breakpointClass: string = "breakpoint";
 
 /*
- 1. No underline (no breakpoint)
- 2. Underline, not selected (black underline)
- 3. Underline, selected (red underline)
+<p class="breakpoint" selected="true" index="7" pseudo="4">
 
-
- #1 -> 3, #(old 3 -> 2)
- #2 -> 3, (old 3 -> 2)
- #3 -> 1
-
+<p selected="true" index="4" code="7">
 */
 
 function breakpoint(code: Element): void
@@ -17,32 +11,45 @@ function breakpoint(code: Element): void
     const lines: NodeListOf<HTMLParagraphElement> = code.querySelectorAll("p");
     for (let i: number = 0; i < lines.length; i++)
     {
-        lines[ i ].addEventListener("dblclick", function (ev: MouseEvent)
-        {
-            document.getSelection().removeAllRanges();
-        });
-
-        lines[ i ].addEventListener("click", function ()
-        {
-            const selected: HTMLElement = document.getElementById("selected");
-
-            if (!(lines[ i ].classList.contains(breakpointClass))) // No underline
-            {
-                lines[ i ].classList.add(breakpointClass);
-                lines[ i ].id = "selected";
-
-                selected.id = "";
-            }
-            else if (lines[ i ].classList.contains(breakpointClass) && lines[ i ].id === "") // Underline, not selected
-            {
-                lines[ i ].id = "selected";
-                selected.id = "";
-            }
-            else if (lines[ i ].classList.contains(breakpointClass) && lines[ i ].id === "selected") // Underline, selected
-            {
-                lines[ i ].classList.remove(breakpointClass);
-                lines[ i ].id = "";
-            }
-        });
+        lines[ i ].addEventListener("dblclick", statementOnDblClick);
+        lines[ i ].addEventListener("click", () => statementOnClick(lines[ i ]));
     }
+}
+
+function statementOnDblClick(): void
+{
+    document.getSelection().removeAllRanges();
+}
+
+function statementOnClick(line: HTMLElement): void
+{
+    if (line.classList.contains(breakpointClass))
+    {
+        if (line.id === "selected") // Breakpoint, selected
+        {
+            // Remove breakpoint
+            line.classList.remove(breakpointClass);
+            line.id = "";
+        }
+        else // Breakpoint, not selected
+        {
+            // Select
+            select(line);
+        }
+    }
+    else // No breakpoint
+    {
+        // Add breakpoint
+        line.classList.add(breakpointClass);
+        // Select
+        select(line);
+    }
+}
+
+
+function select(line: HTMLElement): void
+{
+    const selected: HTMLElement = document.getElementById("selected");
+    line.id = "selected";
+    selected.id = "";
 }
