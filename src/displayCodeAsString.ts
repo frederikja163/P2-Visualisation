fa6d04254d9ad990aaa3f0319c84fd144fd76939
anderszeic <anderszeic@gmail.com>
2022-03-18 10:30:45 +0100

    still same but in a function

--- src/displayCodeAsString.ts
+++ src/displayCodeAsString.ts
@@ -1,36 +1,38 @@
-// function that should be printed
-function printFunction(name: string){
-    name = "Jesper hansen";
-    let a : number = 7;
-    let b : number = 6;
-    console.log(a + b);
-    if (a === b){
-        a++;
-    } 
-    const c = a + b;
-}
-// convert function to string
-let functionString : string = printFunction.toString();
-// split string into array contatining each line as separate string
-let stringArray : Array<string> = functionString.split(/(?<=\{\})|[\r\n]+/);
-// iterate though each line of code, count size of indent
-let indentArray : Array<number> = [];
-for(let i : number = 0; i < stringArray.length ; i++){ 
-    let j : number = 0;
-    let currString : string = String(stringArray[i]);
-    while (currString[j] === " "){
-        j++;
+function displayCodeAsString(){    
+    // function that should be printed
+    function printFunction(name: string){
+        name = "Jesper hansen";
+        let a : number = 7;
+        let b : number = 6;
+        console.log(a + b);
+        if (a === b){
+            a++;
+        } 
+        const c = a + b;
     }
-    indentArray[i] = j;
-    // delete indentation from each line of code
-    currString = currString.substring(indentArray[i]);
+    // convert function to string
+    let functionString : string = printFunction.toString();
+    // split string into array contatining each line as separate string
+    let stringArray : Array<string> = functionString.split(/(?<=\{\})|[\r\n]+/);
+    // iterate though each line of code, count size of indent
+    let indentArray : Array<number> = [];
+    for(let i : number = 0; i < stringArray.length ; i++){ 
+        let j : number = 0;
+        let currString : string = String(stringArray[i]);
+        while (currString[j] === " "){
+            j++;
+        }
+        indentArray[i] = j;
+        // delete indentation from each line of code
+        currString = currString.substring(indentArray[i]);
 
-    // insert indentation outside of <p> tag
-    stringArray[i] = "&nbsp;".repeat(indentArray[i]) + "<p>" + currString + "</p></br>";
+        // insert indentation outside of <p> tag
+        stringArray[i] = "&nbsp;".repeat(indentArray[i]) + "<p>" + currString + "</p></br>";
+    }
+    // create single string from array of lines
+    let paragraphString = stringArray.join("");
+    // display string on website
+
+    let left = document.querySelector("#left")
+    left.innerHTML = "<pre>" + paragraphString + "</pre>";
 }
-// create single string from array of lines
-let paragraphString = stringArray.join("");
-// display string on website
-
-let left = document.querySelector("#left")
-left.innerHTML = "<pre>" + paragraphString + "</pre>";
--- src/script.js
+++ src/script.js
@@ -12,32 +12,34 @@ function breakpoint(code) {
         });
     }
 }
-function printFunction(name) {
-    name = "Jesper hansen";
-    let a = 7;
-    let b = 6;
-    console.log(a + b);
-    if (a === b) {
-        a++;
+function displayCodeAsString() {
+    function printFunction(name) {
+        name = "Jesper hansen";
+        let a = 7;
+        let b = 6;
+        console.log(a + b);
+        if (a === b) {
+            a++;
+        }
+        const c = a + b;
     }
-    const c = a + b;
-}
-let functionString = printFunction.toString();
-let stringArray = functionString.split(/(?<=\{\})|[\r\n]+/);
-let indentArray = [];
-for (let i = 0; i < stringArray.length; i++) {
-    let j = 0;
-    let currString = String(stringArray[i]);
-    while (currString[j] === " ") {
-        j++;
+    let functionString = printFunction.toString();
+    let stringArray = functionString.split(/(?<=\{\})|[\r\n]+/);
+    let indentArray = [];
+    for (let i = 0; i < stringArray.length; i++) {
+        let j = 0;
+        let currString = String(stringArray[i]);
+        while (currString[j] === " ") {
+            j++;
+        }
+        indentArray[i] = j;
+        currString = currString.substring(indentArray[i]);
+        stringArray[i] = "&nbsp;".repeat(indentArray[i]) + "<p>" + currString + "</p></br>";
     }
-    indentArray[i] = j;
-    currString = currString.substring(indentArray[i]);
-    stringArray[i] = "&nbsp;".repeat(indentArray[i]) + "<p>" + currString + "</p></br>";
+    let paragraphString = stringArray.join("");
+    let left = document.querySelector("#left");
+    left.innerHTML = "<pre>" + paragraphString + "</pre>";
 }
-let paragraphString = stringArray.join("");
-let left = document.querySelector("#left");
-left.innerHTML = "<pre>" + paragraphString + "</pre>";
 window.onload = main;
 function main() {
     breakpoint(document.body);
