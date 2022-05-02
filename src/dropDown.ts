1a37a2cf14e1f553835e8d47ad70dcf9a926d319
anderszeic <anderszeic@gmail.com>
2022-05-02 10:27:21 +0200

    cleanup

--- src/dropDown.ts
+++ src/dropDown.ts
@@ -1,15 +1,4 @@
-/*  TODO:maybe create an array with all the algorithms instead so we only have to add a new algorithm in two places:
-        1. In the array (preferably the array should be in a seperate file, maybe algorithms/algorithms.ts)
-        2. Add the algorithm code in the correct algorithms/alg.ts file.
-    Instead of the current system where it has to be added in:
-        1. index.html
-        2. algorithms/
-        3. Add 4 lines in dropdown.ts
-*/
-
-function initDropDown(){
-        
-    
+function initDropDown(){ 
     const left: HTMLElement | null = document.querySelector("#left");
     const dropdownBtn = document.querySelector(".dropbtn");
     const dropdownContent = document.querySelector(".dropdown-content");
